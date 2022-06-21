1fe480e407dca408e6cecedc9c12fb66d6623a01
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-21 14:52:39 +0200

    Add NavBar

--- index.js
+++ index.js
@@ -85,9 +85,14 @@ function Navbar() {
 // Afterward, render it below the Navbar (which
 // you can do inside the ReactDOM.render call below)
 
+function MainContent() {
+  return <h1>I'm learning React!</h1>;
+}
+
 ReactDOM.render(
   <div>
     <Navbar />
+    <MainContent />
   </div>,
   document.getElementById('root')
 );
