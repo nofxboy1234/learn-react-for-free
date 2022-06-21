390ffb7297795286f7ad1bdc3f49cdd1b5ad523c
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-21 15:33:30 +0200

    Render multiple elements in React

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const h1 = document.createElement('h1');
-h1.textContent = 'This is an imperative way to program';
-h1.classList.toggle('header');
-let root = document.getElementById('root');
-root.appendChild(h1);
+// const h1 = document.createElement('h1');
+// h1.textContent = 'This is an imperative way to program';
+// h1.classList.toggle('header');
+// let root = document.getElementById('root');
+// root.appendChild(h1);
+
+ReactDOM.render(
+  <div>
+    <ul>
+      <li>Thing 1</li>
+      <li>Thing 2</li>
+    </ul>
+  </div>,
+  document.getElementById('root')
+);
